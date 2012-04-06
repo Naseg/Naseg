33f01cd2e39677b0c8b4b7ee66d5ae3566211aea
Pollo <pollo1_91@yahoo.it>
2012-04-06 15:11:13 +0200

    aggiunta prima prova del db funzionante

--- courses/app/controllers/Application.java
+++ courses/app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import models.Country;
 import views.html.*;
 
 public class Application extends Controller {
@@ -10,5 +11,10 @@ public class Application extends Controller {
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
-  
+
+  public static Result provadb() {
+    return ok(
+      provadb.render(Country.all())
+      );
+  }
 }
