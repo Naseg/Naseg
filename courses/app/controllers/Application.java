9c24e1ab35e8f9e14edf03736631372103f2d088
fabbo <reinardo.roodies@gmail.com>
2012-05-04 17:17:38 +0200

    fix permessi aggiunta corsi

--- courses/app/controllers/Application.java
+++ courses/app/controllers/Application.java
@@ -10,7 +10,6 @@ import play.data.Form;
 import models.FormData;
 
 public class Application extends Controller {
-  static Form<Course> courseForm = form(Course.class);
   
   public static Result index() {
     return ok(nonAuthIndex.render());
@@ -20,20 +19,6 @@ public class Application extends Controller {
     return ok(
       provadb.render(Country.all(),Student.all())
       );
-  }
-
-  public static Result blankCourseForm() {
-    return ok(courseform.render(courseForm));
-  }
-
-  public static Result newCourse() {
-    Form<Course> filledForm = courseForm.bindFromRequest();
-    if(filledForm.hasErrors()) {
-        return badRequest(courseform.render(filledForm));
-    } else {
-        Course.create(filledForm.get());
-        return ok("creato");
-    }
   }
 }
 
--- courses/app/controllers/SecuredApplication.java
+++ courses/app/controllers/SecuredApplication.java
@@ -11,18 +11,34 @@ import models.FormData;
 
 @Security.Authenticated(Secured.class)
 public class SecuredApplication extends Controller {
+
+  static Form<Course> courseForm = form(Course.class);
     
     public static Result index() {
-    
+
     //System.out.println("***REQ***\n" + request().username());
     //System.out.println("username:" + request().username());
     //System.out.println("uri:" + request().uri());
-    
+
     UserCredentials uc = UserCredentials.find.where().eq("userName",request().username()).findUnique();
-    
+
     return ok(authIndex.render(uc));
-    
-  }
+
+    }
+
+    public static Result blankCourseForm() {
+        return ok(courseform.render(courseForm));
+    }
+
+    public static Result newCourse() {
+        Form<Course> filledForm = courseForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(courseform.render(filledForm));
+        } else {
+            Course.create(filledForm.get());
+            return ok("creato");
+        }
+    }
   
 }
 
