ffea9aad1380d6315d5a78c5df6bfe21041e608c
Pollo <pollo1_91@yahoo.it>
2012-05-13 16:42:02 +0200

    aggiunta cartella test con alcuni esempi

---
+++ courses/test/ModelTest.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+
+import java.util.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.libs.F.*;
+
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+import models.*;
+
+import com.avaje.ebean.*;
+
+public class ModelTest {
+    private String formatted(Date date) {
+        return new java.text.SimpleDateFormat("yyyy-MM-dd").format(date);
+    }
+
+    @Test
+    public void findById() {
+        running(fakeApplication(), new Runnable() {
+           public void run() {
+               Student st = Student.find.byId(1L);
+               assertThat(st.lastName).isEqualTo("Smith");
+               assertThat(formatted(st.dateOfBirth)).isEqualTo("1998-10-12");
+	       Supervisor su = Supervisor.find.byId(1L);
+	       assertThat(su.lastName).isEqualTo("Doe");
+	   }
+        });
+    }
+
+    @Test
+    public void studyPlan() {
+        running(fakeApplication(), new Runnable() {
+           public void run() {
+               Student st = Student.find.byId(1L);
+	       List<Course> plan = st.getStudyPlan();
+	       Assert.assertTrue(plan.size() >= 0);
+	   }
+        });
+    }
+}
