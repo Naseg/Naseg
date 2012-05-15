78aa25e59ebd1db145110e1997f41b0aecf98599
Pollo <pollo1_91@yahoo.it>
2012-05-15 16:50:41 +0200

    spaccato test

--- courses/test/ModelTest.java
+++ courses/test/ModelTest.java
@@ -1,5 +1,5 @@
 package test;
-
+import java.io.*;
 import org.junit.*;
 
 import java.util.*;
@@ -20,43 +20,41 @@ public class ModelTest {
     return new java.text.SimpleDateFormat("yyyy-MM-dd").format(date);
   }
 
-  @Test
-  public void fakeTest() {
-    running(fakeApplication(), new Runnable() {
-	public void run() {
-	  boolean condition = false;
-	  UserRole ur = new UserRole();
-	  ur.role = "ruolo_prova";
-	  ur.deleted = false;
-	  List<UserRole> al = UserRole.all();
-	  for (UserRole u : al)
-	    if (u.role == "ruolo_prova")
-	      condition = true;
-	  Assert.assertTrue(condition);
-	}
-      });
-  }
+  /*try {
+    FileOutputStream file = new FileOutputStream("/tmp/file.txt");
+    PrintStream Output = new PrintStream(file);
+    Output.println("");
+    } catch (IOException e) {}*/
 
   @Test
-  public void findById() {
+  public void getStudyPlan() {
     running(fakeApplication(), new Runnable() {
 	public void run() {
-	  Student st = Student.find.byId(1L);
-	  assertThat(st.lastName).isEqualTo("Smith");
-	  assertThat(formatted(st.dateOfBirth)).isEqualTo("1998-10-12");
-	  Supervisor su = Supervisor.find.byId(1L);
-	  assertThat(su.lastName).isEqualTo("Doe");
-	}
-      });
-  }
-
-  @Test
-  public void studyPlan() {
-    running(fakeApplication(), new Runnable() {
-	public void run() {
-	  Student st = Student.find.byId(1L);
+	  Student st = new Student();
+	  st.save();
+	  try{
+	    // Create file
+	    FileWriter fstream = new FileWriter("/tmp/out.txt");
+	    BufferedWriter out = new BufferedWriter(fstream);
+	    out.write("Hello Java");
+	    //Close the output stream
+	    out.close();
+	  }catch (Exception e){//Catch exception if any
+	    System.err.println("Error: " + e.getMessage());
+	  }
+          //for (Student t : Student.all()) { Output.println(t); }
+	  Course c = new Course();
+	  c.academicYear = Course.AcademicYear();
+	  c.save();
+	  CourseEnrollment ce = new CourseEnrollment();
+	  ce.student = st;
+	  ce.course = c;
+	  ce.save();
 	  List<Course> plan = st.getStudyPlan();
-	  Assert.assertTrue(plan.size() >= 0);
+	  Assert.assertTrue(true);
+	  st.delete();
+	  c.delete();
+	  ce.delete();
 	}
       });
   }
